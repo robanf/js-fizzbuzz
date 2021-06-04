730cb85c0892bc18fd58aa80136fd2858425ef3d
Roberto <robertoanfuso98@gmail.com>
2021-06-04 09:44:26 +0200

    corretto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-for(var i=1; i<101;i++){
+/* for(var i=1; i<101;i++){
     var stampa="";
     var esiste=false;
     if(i%3==0){
@@ -11,7 +11,30 @@ for(var i=1; i<101;i++){
     }
 
     if(esiste){
-        document.getElementById("num").innerHTML+= i + stampa+ "<br>"
+        document.getElementById("num").innerHTML+= i + stampa+ "<br>";
     }
 
+} */
+
+
+
+
+
+/* Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi anche "Fizz" di fianco al numero e per i multipli di 5 stampi "Buzz". Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz". :collisione: */
+
+for(var i=1;i<=100;i++){
+    console.log(i);
+    var stampa="";
+    if(i%3==0){
+        stampa+="Fizz";
+    }
+    if(i%5==0){
+        stampa+="Buzz";
+    }
+
+    
+    document.getElementById("num").innerHTML+= i+stampa+"<br>";
+
 }
+
+
